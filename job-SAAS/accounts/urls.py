b2cca42a9e2e315836eeac707aa910f351e8d5fb
AkhileshGupta-2003 <akhilesh.bimal@gmail.com>
2025-12-27 12:09:33 +0530

    added and repaired urls.py

--- job-SAAS/accounts/urls.py
+++ job-SAAS/accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from .views import signup
-urlatterns = [
+urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'acounts'),name = 'logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'),name = 'logout'),
     path('signup/', signup, name = 'signup')
 
 ]
