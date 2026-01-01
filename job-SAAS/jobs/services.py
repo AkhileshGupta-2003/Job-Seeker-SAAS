90e53adea011c3272ac84f3c8711b522eb66ff27
AkhileshGupta-2003 <akhilesh.bimal@gmail.com>
2026-01-01 18:45:42 +0530

    created the job seraching agent

--- job-SAAS/jobs/services.py
+++ job-SAAS/jobs/services.py
@@ -1,16 +1,28 @@
 import os , time
 import json
 import requests
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+
+
+
+
+
+
+
+@tool('search_jobs_on_linkedin', description = 'Search jobs on linkedin for job listings using parameters (location , keyword, country,job_type, experience_level, on_site, comapny, location_radius) based on user input. This functions returns all job listings found on linkedin ')
 def search_jobs_on_linkedin(location :str,
                             keyword: str,
                             country : str,
-                            time_range: str,
                             job_type: str,
                             experience_level : str,
                             on_site : str,
                             company: str,
-                            location_radius:str):
-    url = 'https://api.brightdata.com/datasets/V3/trigger'
+                            location_radius:str,
+                            time_range: str = 'past_month'):
+    
+    url = '"https://api.brightdata.com/datasets/v3/scrape?dataset_id=gd_lpfll7v5hcqtkxl6l&notify=false&include_errors=true&type=discover_new&discover_by=keyword&limit_per_input=5"'
     headers = {
     "Authorization": "Bearer BRIGHT_DATA_SECRET_KEY",
     "Content-Type": "application/json"}
@@ -38,3 +50,43 @@ def search_jobs_on_linkedin(location :str,
     return response.json()
 
 
+
+
+
+@tool('search_jobs_on_glassdoor', description = 'Search jobs on Glassdoor for job listings using parameters (location , keyword, country) based on user input. This function returns all the job listings found on glassdoor.')
+def search_jobs_on_glassdoor(location :str,
+                            keyword: str,
+                            country : str,
+                            time_range: str = 'past_month'):
+    url = '"https://api.brightdata.com/datasets/v3/scrape?dataset_id=gd_l7j0bx501ockwldaqf&notify=false&include_errors=true&type=discover_new&discover_by=keyword&limit_per_input=5"'
+    headers = {
+    "Authorization": "Bearer BRIGHT_DATA_SECRET_KEY",
+    "Content-Type": "application/json"}
+
+    data = json.dumps({"input": [{
+        "location":location,
+        "keyword":keyword,
+        "country":country}]})
+
+    response = requests.post(url,headers = headers, data = data)
+    response.raise_for_status()
+    snapshot_id = response.json()['snapshot_id']
+    url = f'https://api.brightdata.com/datasets/V3/trigger/{snapshot_id}'
+    while requests.get(url, headers = headers).json()['status'] != 'ready':
+        time.sleep(5)
+    url = f'https://api.brightdata.com/datasets/V3/trigger/{snapshot_id}?format=json'
+    response = requests.get(url,headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def search_jobs_agent(prompt : str) -> str: 
+    agent = create_agent(
+        model = 'gpt-4.1-mini',
+        tools = [search_jobs_on_linkedin, search_jobs_on_glassdoor]
+    )
+    response = agent.invoke(
+        {'messages':
+            [{'role' : 'system' , 'content':' You are a helpful assistant that helps users search for job listings on linkedin and glassdoor based on their preferences'},
+             {'role' : 'user', 'content' : prompt}]}
+    )
+    return response['messages'][-1].content
